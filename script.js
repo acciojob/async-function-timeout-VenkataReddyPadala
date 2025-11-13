d885e86ea6a061e4f356cf379ea5c99791e892f7
VenkataReddyPadala <84061954+VenkataReddyPadala@users.noreply.github.com>
2025-11-13 19:28:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const textEle = document.querySelector("#text");
+const delayEle = document.querySelector("#delay");
+const btn = document.querySelector("#btn");
+const outputEle = document.querySelector("#output");
+
+btn.addEventListener('click',async ()=>{
+		await new Promise(resolve=>setTimeout(resolve,delayEle.value.trim()))
+		outputEle.textContent=`${textEle.value.trim()}`
+})
